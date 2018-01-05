7015580e329f076a9dd72d5853e6110ff1850032
Roy Montoya <roymontoya89@gmail.com>
2018-01-05 12:42:02 -0600

    working app again with tabs

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,12 +2,19 @@ import React, {Component} from 'react';
 import Dishes from './Dishes';
 import Drinks from './Drinks'
 import UserOrders from './UserOrders';
+import {Link} from 'react-router'
 
 class Canvas extends Component {
   render() {
     return (<div className="container">
       <div className="jumbotron">
         <h2>Nombre de Tienda</h2>
+      </div>
+      <div>
+        <ul className = "nav nav-pills ListMargin">
+          <li role = "presentation" className = "active"><Link to = "/">Dishes</Link></li>
+          <li role = "presentation" className = "active"><Link to = "drinks">Drinks</Link></li>
+        </ul>
       </div>
       {this.props.children}
       <UserOrders/>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Counter from './Counter';
 import Canvas from './components/Canvas'
 import Dishes from './components/Dishes'
+import Drinks from './components/Drinks'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './App';
 import './index.css';
@@ -11,6 +12,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 const tabs = (<Router  history = {browserHistory}>
   <Route path = "/" component= {Canvas} >
     <IndexRoute component = {Dishes}></IndexRoute>
+    <Route path = "drinks" component = {Drinks}></Route>
   </Route>
 </Router>);
 
